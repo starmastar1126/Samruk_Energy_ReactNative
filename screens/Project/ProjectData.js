dc4dc196d73377d9d9d883e9d2f1d3f4fa49eb66
Silas Jo <52027648+starmaster1126@users.noreply.github.com>
2020-07-17 05:12:23 +0200

    zother

--- screens/Project/ProjectData.js
+++
@@ -1,9 +0,0 @@
-import React, { PureComponent } from "react";
-import { View } from "react-native";
-import HTMLView from "react-native-htmlview";
-
-export default class ProjectData extends PureComponent {
-  render() {
-    return <HTMLView value={this.props.data.publication} />;
-  }
-}
